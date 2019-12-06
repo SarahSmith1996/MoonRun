92656b7de465733e6f30552c2f2ec62dd77da7f3
Sarah Smith <sarahl.smith@hotmail.com>
2019-12-06 12:06:27 +0000

    Made Images parent class
    Images parent class to background

--- Display.py
+++ Display.py
@@ -4,28 +4,6 @@ pygame.init()
 class Display ():
     pass
 
-class Images (Display):
-    
-    def __init__(self):
-        pass
-
-    imgdict = {"night": pygame.image.load('starry.png'),
-            "background": pygame.image.load('hills_bg.png'),
-            "info1": pygame.image.load('info1.png'),
-            "info2" : pygame.image.load('info2.png'), 
-            "p1move" : [pygame.image.load('p11.png'), pygame.image.load('p12.png'),\
-                        pygame.image.load('p13.png'), pygame.image.load('p14.png'), \
-                        pygame.image.load('p15.png'), pygame.image.load('p16.png'),\
-                        pygame.image.load('p17.png'), pygame.image.load('p18.png')], 
-            "p2move" : [pygame.image.load('p21.png'), pygame.image.load('p22.png'),\
-                        pygame.image.load('p23.png'), pygame.image.load('p24.png'),\
-                        pygame.image.load('p25.png'), pygame.image.load('p26.png'), \
-                        pygame.image.load('p27.png'), pygame.image.load('p28.png')],
-            "p1stand" : pygame.image.load('p10.png'),
-            "p1jump" : [pygame.image.load('p1j.png'),pygame.image.load('p1j2.png')],
-            "p2stand" : pygame.image.load('p20.png'),
-            "p2jump" : [pygame.image.load('p2j.png'),pygame.image.load('p2j2.png')]}
-
     def get_image (self, imgname):
         self.imgname = imgname
         if self.imgname in self.imgdict:
@@ -60,7 +38,30 @@ class Colours (Display):
     p1colour = pygame.Color('red')
     p2colour = pygame.Color('blue')
 
-class Background (Screen):
+class Images (Display):
+    
+    def __init__(self):
+        pass
+
+    imgdict = {"night": pygame.image.load('starry.png'),
+            "background": pygame.image.load('hills_bg.png'),
+            "info1": pygame.image.load('info1.png'),
+            "info2" : pygame.image.load('info2.png'), 
+            "p1move" : [pygame.image.load('p11.png'), pygame.image.load('p12.png'),\
+                        pygame.image.load('p13.png'), pygame.image.load('p14.png'), \
+                        pygame.image.load('p15.png'), pygame.image.load('p16.png'),\
+                        pygame.image.load('p17.png'), pygame.image.load('p18.png')], 
+            "p2move" : [pygame.image.load('p21.png'), pygame.image.load('p22.png'),\
+                        pygame.image.load('p23.png'), pygame.image.load('p24.png'),\
+                        pygame.image.load('p25.png'), pygame.image.load('p26.png'), \
+                        pygame.image.load('p27.png'), pygame.image.load('p28.png')],
+            "p1stand" : pygame.image.load('p10.png'),
+            "p1jump" : [pygame.image.load('p1j.png'),pygame.image.load('p1j2.png')],
+            "p2stand" : pygame.image.load('p20.png'),
+            "p2jump" : [pygame.image.load('p2j.png'),pygame.image.load('p2j2.png')]}
+
+
+class Background (Images):
 
     def __init__(self,x,y,vel,img,width,height):
         
